58edf21b45f662fd2b08dd01fd2cab765b54157a
swami303 <rakeshthummar2012@gmail.com>
2018-07-14 13:54:57 +0530

    commit
    commit

---
+++ demoFramework/demoFramework.h
@@ -0,0 +1,19 @@
+//
+//  demoFramework.h
+//  demoFramework
+//
+//  Created by Brainstorm on 14/07/18.
+//  Copyright © 2018 Brainstorm. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for demoFramework.
+FOUNDATION_EXPORT double demoFrameworkVersionNumber;
+
+//! Project version string for demoFramework.
+FOUNDATION_EXPORT const unsigned char demoFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <demoFramework/PublicHeader.h>
+
+
---
+++ demoFramework/swiftFIle.swift
@@ -0,0 +1,9 @@
+//
+//  swiftFIle.swift
+//  demoFramework
+//
+//  Created by Brainstorm on 14/07/18.
+//  Copyright © 2018 Brainstorm. All rights reserved.
+//
+
+import Foundation
